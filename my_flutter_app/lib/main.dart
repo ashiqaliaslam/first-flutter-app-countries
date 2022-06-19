ccc0c9cf4b91fa47bd46d0fffdf907338eb08410
ali <alitechbeyond@gmail.com>
2022-06-19 22:15:05 +0500

    flash card exercise done

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/countries.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() {
   runApp(
@@ -6,51 +8,150 @@ void main() {
   );
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MyBulbApp();
+    return MaterialApp(home: MyCountryApp());
   }
 }
 
-class MyBulbApp extends StatefulWidget {
+// ignore: use_key_in_widget_constructors
+class MyCountryApp extends StatefulWidget {
   @override
-  State<MyBulbApp> createState() => _MyBulbAppState();
+  State<MyCountryApp> createState() => _MyCountryAppState();
 }
 
-class _MyBulbAppState extends State<MyBulbApp> {
-  bool isTurnedOn = false;
+class _MyCountryAppState extends State<MyCountryApp> {
+  int score = 0;
+  int totalAttempted = 0;
+  bool showAnswer = false;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Lets Glow'),
-          backgroundColor: Colors.blueAccent,
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+    return Scaffold(
+      backgroundColor: Colors.lightBlue.shade100,
+      appBar: AppBar(
+        centerTitle: true,
+        // ignore: prefer_const_constructors
+        title: Text('Guess the Capital City!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            score = 0;
+            totalAttempted = 0;
+          });
+        },
+        // ignore: prefer_const_constructors
+        child: Text('Reset'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          // ignore: prefer_const_constructors
+          Center(
+            child: Text(
+              'Score ${score}/${totalAttempted}',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+          Padding(
+            // ignore: prefer_const_constructors
+            padding: EdgeInsets.all(20),
+            child: SizedBox(
+              height: 200,
+              child: Card(
+                elevation: 20,
+                shadowColor: Colors.grey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      showAnswer ? 'Capital' : 'Country',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    Text(
+                      showAnswer
+                          ? countries[totalAttempted]['city']!
+                          : countries[totalAttempted]['country']!,
+                      style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.blueAccent),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            // width: 50,
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showAnswer = !showAnswer;
+                  });
+                },
+                child: Text('Show ${showAnswer ? 'Answer' : 'Question'}'),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Icon(
-                isTurnedOn ? Icons.lightbulb : Icons.lightbulb_outline,
-                size: 200,
-                color: isTurnedOn ? Colors.yellow : Colors.black,
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (totalAttempted < countries.length - 1) {
+                      score++;
+                      totalAttempted++;
+                    } else {
+                      Alert(
+                        context: context,
+                        title: 'ALERT',
+                        desc: 'You already have reached to the end of list.',
+                      ).show();
+                    }
+                  });
+                },
+                child: const Text('Correct'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                ),
               ),
               ElevatedButton(
-                  onPressed: () {
-                    print('button pressed');
-                    setState(() {
-                      isTurnedOn = !isTurnedOn;
-                    });
-                  },
-                  child: Text(!isTurnedOn ? 'Turn On' : 'Turn Off')),
+                onPressed: () {
+                  setState(() {
+                    if (totalAttempted < countries.length - 1) {
+                      totalAttempted++;
+                    } else {
+                      Alert(
+                        context: context,
+                        title: 'ALERT',
+                        desc: 'You already have reached to the end of list.',
+                      ).show();
+                    }
+                  });
+                },
+                child: const Text('Wrong'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                ),
+              ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
